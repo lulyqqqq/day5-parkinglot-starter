adb8f068215afe2a4999e718799ed9ebc01e0594
Mason ma <MAMA14@oocl.com>
2024-11-22 15:23:19 +0800

    [Story3] feat:case2,should return a car when fetch a car given a ticket

--- src/test/java/ParkingBoy.java
+++ src/test/java/ParkingBoy.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingBoy {
@@ -14,5 +15,18 @@ public class ParkingBoy {
         Ticket ticket = packingBoy.park(car);
         //Then
         assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_return_the_car_when_fetch_given_a_ticket() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        PackingBoy packingBoy = new PackingBoy(parkingLot);
+        Car car = new Car();
+        Ticket ticket = packingBoy.park(car);
+        //When
+        Car fetchedCar = packingBoy.fetch(ticket);
+        //Then
+        assertEquals(car, fetchedCar);
     }
 }
