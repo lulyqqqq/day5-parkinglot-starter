7c4752f05aa8a20964d996bbeb37e5b3a11af001
Mason ma <MAMA14@oocl.com>
2024-11-22 18:07:18 +0800

    [Story4] feat:case2,should return parking lot two when parking car and parking tow not fill given a car

--- src/test/java/ParkingLotMultipleTest.java
+++ src/test/java/ParkingLotMultipleTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotMultipleTest {
     @Test
-    public void should_return_parking_lot_one_when_parking_car_and_parking_not_fill_given_a_car(){
+    public void should_return_parking_lot_one_when_parking_car_and_parking_not_full_given_a_car(){
         //Given
         ParkingLot firstParkingLot = new ParkingLot(1);
         ParkingLot secondParkingLot = new ParkingLot(2);
@@ -19,5 +19,26 @@ public class ParkingLotMultipleTest {
         //Then
         assertEquals(car,fetchedCar);
         assertEquals(firstParkingLot.parkingLotId,ticket.getParkingLotId());
+    }
+
+    @Test
+    public void should_return_parking_lot_first_full_and_parking_second_when_parking_car_and_parking_not_full_given_a_car(){
+        //Given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(2);
+        PackingBoy packingBoy = new PackingBoy(firstParkingLot);
+        packingBoy.addParkingLots(secondParkingLot);
+        Car car=  new Car();
+        //When
+        for (int i = 0; i < 10; i++) {
+            packingBoy.park(new Car());
+        }
+        Ticket ticket = packingBoy.park(car);
+
+        Car fetchedCar = packingBoy.fetch(ticket);
+
+        //Then
+        assertEquals(car,fetchedCar);
+        assertEquals(secondParkingLot.parkingLotId,ticket.getParkingLotId());
     }
 }
