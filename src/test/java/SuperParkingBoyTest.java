061318e5937a68f1c2cde9e8e1db68cbb709bf28
Mason ma <MAMA14@oocl.com>
2024-11-24 13:51:58 +0800

    [Story6] feat:case4,should return error message when no parking two car and parking not full given wrong ticket

--- src/test/java/SuperParkingBoyTest.java
+++ src/test/java/SuperParkingBoyTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SuperParkingBoyTest {
     @Test
@@ -73,5 +74,19 @@ public class SuperParkingBoyTest {
 
         assertEquals(secondCar,fetchedCarSecond);
         assertEquals(secondParkingLot.parkingLotId,ticketSecond.getParkingLotId());
+    }
+
+    @Test
+    public void should_return_error_message_when_no_parking_two_car_and_parking_not_full_given_wrong_ticket(){
+        //Given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(2);
+        PackingBoy superParkingBoy = new SuperParkingBoy(firstParkingLot);
+        superParkingBoy.addParkingLots(secondParkingLot);
+        //When
+        Ticket wrongTicket = new Ticket();
+
+        //Then
+        assertThrows(UnrecognizedParkingTicketException.class, () -> superParkingBoy.fetch(wrongTicket), UnrecognizedParkingTicketException.UNRECOGNIZED_PARKING_TICKET);
     }
 }
