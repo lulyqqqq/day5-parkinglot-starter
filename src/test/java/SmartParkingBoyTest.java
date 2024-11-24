bda00b6b752bf32fb8ab8b6685df7a904ee50d78
Mason ma <MAMA14@oocl.com>
2024-11-24 12:41:01 +0800

    [Story5] feat:case3,should return right car ticket when parking two car and parking not full given two car

--- src/test/java/SmartParkingBoyTest.java
+++ src/test/java/SmartParkingBoyTest.java
@@ -50,4 +50,28 @@ public class SmartParkingBoyTest {
         assertEquals(car,fetchedCar);
         assertEquals(secondParkingLot.parkingLotId,ticket.getParkingLotId());
     }
+
+    @Test
+    public void should_return_right_car_ticket_when_parking_two_car_and_parking_not_full_given_two_car(){
+        //Given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(2);
+        PackingBoy smartParkingBoy = new SmartParkingBoy(firstParkingLot);
+        smartParkingBoy.addParkingLots(secondParkingLot);
+        Car fisrtCar =  new Car();
+        //When
+        Ticket ticketFirst = smartParkingBoy.parkInSpecificParkingLot(fisrtCar,1);
+        Car fetchedCarFirst = smartParkingBoy.fetch(ticketFirst);
+
+        //Then
+        assertEquals(fisrtCar,fetchedCarFirst);
+        assertEquals(firstParkingLot.parkingLotId,ticketFirst.getParkingLotId());
+
+        Car secondCar = new Car();
+        Ticket ticketSecond = smartParkingBoy.parkInSpecificParkingLot(secondCar,2);
+        Car fetchedCarSecond = smartParkingBoy.fetch(ticketSecond);
+
+        assertEquals(secondCar,fetchedCarSecond);
+        assertEquals(secondParkingLot.parkingLotId,ticketSecond.getParkingLotId());
+    }
 }
