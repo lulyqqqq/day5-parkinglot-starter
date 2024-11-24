a06f23349a34a9195da4a6412c26ff212585f41e
Mason ma <MAMA14@oocl.com>
2024-11-24 12:34:26 +0800

    [Story5] feat:case2,should return parking lot two id when parking car and parking lot one parking more car given a car

--- src/test/java/SmartParkingBoyTest.java
+++ src/test/java/SmartParkingBoyTest.java
@@ -26,4 +26,28 @@ public class SmartParkingBoyTest {
         assertEquals(car,fetchedCar);
         assertEquals(firstParkingLot.parkingLotId,ticket.getParkingLotId());
     }
+
+    @Test
+    public void should_return_parking_lot_two_id_when_parking_car_and_parking_lot_one_parking_more_car_given_a_car(){
+        //Given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(2);
+        PackingBoy smartParkingBoy = new SmartParkingBoy(firstParkingLot);
+        smartParkingBoy.addParkingLots(secondParkingLot);
+        //When
+        for (int i = 0; i < 5; i++) {
+            smartParkingBoy.parkInSpecificParkingLot(new Car(),1);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            smartParkingBoy.parkInSpecificParkingLot(new Car(),2);
+        }
+
+        Car car = new Car();
+        Ticket ticket = smartParkingBoy.park(car);
+        Car fetchedCar = smartParkingBoy.fetch(ticket);
+        //Then
+        assertEquals(car,fetchedCar);
+        assertEquals(secondParkingLot.parkingLotId,ticket.getParkingLotId());
+    }
 }
