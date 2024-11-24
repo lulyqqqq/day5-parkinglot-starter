a4a9838f8c185ef73407d06811af1e94d06d91c5
Mason ma <MAMA14@oocl.com>
2024-11-24 15:03:27 +0800

    [Story7] feat:case3,should return parking lot one when parking car and parking lot parking rate different given a car

--- src/test/java/StrategyPatternTest.java
+++ src/test/java/StrategyPatternTest.java
@@ -50,5 +50,27 @@ public class StrategyPatternTest {
         assertEquals(secondParkingLot.parkingLotId,ticket.getParkingLotId());
     }
 
+    @Test
+    public void should_return_parking_lot_one_when_parking_car_and_parking_lot_parking_rate_different_given_a_car(){
+        //Given
+        ParkingLot firstParkingLot = new ParkingLot(1,5);
+        ParkingLot secondParkingLot = new ParkingLot(2,7);
+        PackingBoy superParkingBoy = new PackingBoy(firstParkingLot,new SuperParkingBoyStrategy());
+        superParkingBoy.addParkingLots(secondParkingLot);
+        //When
+        for (int i = 0; i < 4; i++) {
+            superParkingBoy.parkInSpecificParkingLot(new Car(),1);
+        }
 
+        for (int i = 0; i < 2; i++) {
+            superParkingBoy.parkInSpecificParkingLot(new Car(),2);
+        }
+
+        Car car = new Car();
+        Ticket ticket = superParkingBoy.park(car);
+        Car fetchedCar = superParkingBoy.fetch(ticket);
+        //Then
+        assertEquals(car,fetchedCar);
+        assertEquals(firstParkingLot.parkingLotId,ticket.getParkingLotId());
+    }
 }
